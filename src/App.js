572f25903c1b7db9377e64c42d136678129c0f35
Thibaut <thibautdebroucke@gmail.com>
2023-03-07 11:29:44 +0100

    Routes1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
+import { Route, Link, Routes } from "react-router-dom";
 import './App.css';
+import Article from './Article/Article';
+import Home from './Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <p>That's our beautiful app.</p>
+      <Link to="/Article"><Article/></Link>
+
+      <Routes>
+        <Route exact={true} path="/" element={<Home/>} />
+        <Route exact={true} path="/Article" element={<Article/>} />
+      </Routes>
     </div>
   );
 }
